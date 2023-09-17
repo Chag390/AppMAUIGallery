c7fb3afc46a715d3289daf533823f97b05db7548
Cláudio Gomes <claudio.chag@gmail.com>
2023-09-17 19:48:53 -0300

    Aula 70 -  Button 1 - Seção 6 / .NET MAUI - Criar Apps para Android, iOS, Mac e Windows. / plataforma UDEMY.

---
+++ Views/Components/Mains/ButtonPage.xaml.cs
@@ -0,0 +1,29 @@
+namespace AppMAUIGallery.Views.Components.Mains;
+
+public partial class ButtonPage : ContentPage
+{
+    public ButtonPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Pressed(object sender, EventArgs e)
+    {
+        LblLog.Text += $"\nPressionado: {DateTime.Now}";
+    }
+
+    private void Button_Released(object sender, EventArgs e)
+    {
+        LblLog.Text += $"\nLiberado: {DateTime.Now}";
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        LblLog.Text += $"\nClicado: {DateTime.Now}";
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        LblLog.Text = "";
+    }
+}
